f3c9b7b7f07d679d6e3a84a2cac881d73233954d
khanhbnph36160 <khanhbnph36160@fpt.edu.vn>
2025-06-19 00:32:41 +0700

    CRUD discount

--- src/main/java/com/example/datn/repositories/CouponRepo.java
+++
@@ -1,7 +0,0 @@
-package com.example.datn.repositories;
-
-import com.example.datn.entities.Coupon;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CouponRepo extends JpaRepository<Coupon,Integer> {
-}
