1345070e8bbaaa0a3f4c11146d9e82fb3eec741b
Buibaotien303 <tienbuibao303@gmail.com>
2025-07-02 23:19:00 +0700

    TienBB code san pham

--- src/main/java/com/example/datn/repositories/product_and_other/ProductRepository.java
+++ src/main/java/com/example/datn/repositories/product_and_other/ProductRepository.java
@@ -1,5 +1,6 @@
 package com.example.datn.repositories.product_and_other;
 
+import com.example.datn.entities.product_and_other.Category;
 import com.example.datn.entities.product_and_other.Product;
 import com.example.datn.entities.product_and_other.ProductDetail;
 import org.springframework.data.domain.Page;
@@ -31,4 +32,10 @@ public interface ProductRepository extends JpaRepository<Product, Integer> {
 
     @Query("SELECT pd FROM ProductDetail pd WHERE pd.product.id = :id")
     List<ProductDetail> findAllProductDetail(Integer id);
+
+    @Query("SELECT p FROM Product p " +
+            "WHERE (LOWER(p.name) LIKE LOWER(CONCAT('%', :name, '%')) " +
+            "AND (:status IS NULL OR p.status = :status)) " +
+            "ORDER BY p.id DESC")
+    Page<Product> search(String name, Boolean status, Pageable pageable);
 }
