9e603c9e0eebb5eebbfe2f57f932a550e4fd472f
TranLinh2911 <linhtnph31789@fpt.edu.vn>
2025-07-06 14:11:21 +0700

    update trang thai sdt khach hang

--- src/main/java/com/example/datn/controllers/BillController.java
+++ src/main/java/com/example/datn/controllers/BillController.java
@@ -1,22 +1,19 @@
-package com.example.datn.controllers;
-
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-
-@Controller
-<<<<<<< HEAD:src/main/java/com/example/datn/controllers/indexController.java
-//@RequestMapping
-public class indexController {
-    @GetMapping
-    public String hienThiTrangChu() {
-        return "admin/index";
-=======
-@RequestMapping("/admin/bill")
-public class BillController {
-    @GetMapping("/view")
-    public String billView(){
-        return "admin/bill";
->>>>>>> 96abaefb9b462e2fe9335df8722f6dea85121bdc:src/main/java/com/example/datn/controllers/BillController.java
-    }
-}
+//package com.example.datn.controllers;
+//
+//import org.springframework.stereotype.Controller;
+//import org.springframework.web.bind.annotation.GetMapping;
+//import org.springframework.web.bind.annotation.RequestMapping;
+//
+//@Controller
+////@RequestMapping
+//public class indexController {
+//    @GetMapping
+//    public String hienThiTrangChu() {
+//        return "admin/index";
+//@RequestMapping("/admin/bill")
+//public class BillController {
+//    @GetMapping("/view")
+//    public String billView(){
+//        return "admin/bill";
+//    }
+//}
