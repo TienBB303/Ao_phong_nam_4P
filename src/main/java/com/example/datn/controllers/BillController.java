f165465b4a31fea9d954868fe8c166a5b0d64d05
khanhbnph36160 <khanhbnph36160@fpt.edu.vn>
2025-07-14 00:02:36 +0700

    Update BILL

--- src/main/java/com/example/datn/controllers/BillController.java
+++ src/main/java/com/example/datn/controllers/BillController.java
@@ -1,17 +1,33 @@
 
 package com.example.datn.controllers;
 
+import com.example.datn.entities.Bill;
+import com.example.datn.repositories.BillRepository;
+import com.example.datn.services.BillService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.List;
 
 @Controller
 @RequestMapping("admin/bill")
 public class BillController {
-
+     @Autowired
+    BillService billService;
     @GetMapping("/view")
-    public String index() {
-
+    public String index( @RequestParam(defaultValue = "0") int page,
+                         @RequestParam(defaultValue = "5") int size,
+            Model model) {
+        Page<Bill> billPage = billService.getAllBills(PageRequest.of(page, size));
+        model.addAttribute("bills", billPage.getContent());
+        model.addAttribute("currentPage", page);
+        model.addAttribute("totalPages", billPage.getTotalPages());
         return "admin/bill";
     }
 }
