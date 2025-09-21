c3759eb61f3ff43b4f864dadf8a115be99545ed1
TranLinh2911 <linhtnph31789@fpt.edu.vn>
2025-09-21 13:10:53 +0700

    Add CustomAuthenticationFailureHandler và cập nhật các file liên quan

--- src/main/java/com/example/datn/controllers/selling/SellingInlineController.java
+++ src/main/java/com/example/datn/controllers/selling/SellingInlineController.java
@@ -387,12 +387,12 @@ public class SellingInlineController {
             response.put("address", bill.getAddress_shipping());
 
             return ResponseEntity.ok(response);
-//        }catch (Exception e){
-//            throw new RuntimeException(e);
-//        }
-        } catch (Exception e) {
-            return ResponseEntity.badRequest().body(e.getMessage());
+        }catch (Exception e){
+            throw new RuntimeException(e);
         }
+//        } catch (Exception e) {
+//            return ResponseEntity.badRequest().body(e.getMessage());
+//        }
     }
 
     @PostMapping("/remove-customer-from-cart")
